82f9bbf7e8ba42624b0c3aeb9605b91bfcbb65a5
Loryerba <lorrenzoerba250@gmail.com>
2024-03-18 10:47:22 +0100

    New version, fix some bugs. Last Update

--- CatalanoFioreVC(rsa_version).py
+++ CatalanoFioreVC(rsa_version).py
@@ -1,147 +1,138 @@
+import math
 from Crypto.Util import number
-from Crypto.Random import random
-import time
-from functools import reduce
-import operator
-import sys
-from sympy.ntheory import totient
-from sympy.polys.polytools import gcd
-from egcd import egcd
+import copy
 
+#verify fun
+def verify(C, message, i, open, S, ei_list, n):
 
-sys.set_int_max_str_digits(0)
-start_time = time.time()
+    #evaluate proof^ei-th % n
+    proofToEi =pow(open, ei_list[i], n)
+    #evaluate si-th^message * (proofToEi)
+    newC = proofToEi*pow(S[i], message, n)%n
 
-#global public parameters
-N: int
-g: int
-si: list
-ei : list 
+    #returns 1 if two commitment are equals, else 0
+    if(C == newC):
+        return 1
+    else:
+        return 0
 
+#opening fun
+def open(messages, ei_list, a, n, i):
 
-def keyGen(k : int,l : int, q : int):
-    """k correspond to security parameter, must larger than 1
-        l correspond to length of q (l+1)-bit random primes and q is the number of primes e1,...,eq"""
-    #first prime number
-    p1 = 0
-    #second prime number
-    p2 = 0
-    #generating two distinct  k/2-bit primes number
-    while(p1 == p2):
-        p1 = number.getPrime(int(k/2))
-        p2 = number.getPrime(int(k/2))
-    #evaluate N = p1*p2
-    N: int = 0
-    N = p1*p2
-    
-    #set of q l(+1)-bit primes since e1,....,eq do not divide for N
-    ei_primes_list = list ()
-    
-    N_totient = totient(N)
-    while(0 <= len(ei_primes_list) < q):
-        #we can use := inside larger expression like if statement since Python 3.8
-        if(N_totient % (ei := number.getPrime(l+1))  != 0):
-            ei_primes_list.append(ei)
-    
-    si_list = list()
-    
-    #private function for calcutating generator of cyclic group Zn which has N-order 
-    def __get_generator(order: int):
-        s = set(range(1,order))
-        for g_element in s:
-            element_generated = set()
-            for i in s:
-                element_generated.add((g_element**i)%order)
-            if element_generated == s:
-                return g_element
-
-        
-    #get first generator of Zn
-    #g: int = __get_generator(N)
-    g: int = random.randrange(1,N)
-    #evaluate Si value 
-    for i in range(0,q):
-        ei_primes_list_copy = list(ei_primes_list)
-        ei_primes_list_copy.pop(i)
-        si_list.append(g**reduce(operator.mul,ei_primes_list_copy))
-        
-           
-        
-    
-    return N,g,si_list,ei_primes_list
-
-
-#committing procedure. Return the commitment value C and aux information (message:list)
-
-def com(message : list):
-
-    C : int = 1
-    for i in range(0,len(si)):
-        C = C * (si[i]**int.from_bytes(message[i].encode(),sys.byteorder))
-    return C
-
-
-#opening procedure
-def open(m:str,i:int,aux:list):
-
-    
-    #evaluate moduler ethroot of message
-    def __ethRoot(e: int, message: int, m: int):
-        m_totient = totient(m)
-        if(gcd(e,m_totient) == 1 and gcd(message,m) ==1):
-            (gcd_,t,s) = egcd(e,m_totient)
-            power = message ** t
-            remainder = power % m
-            if((remainder**e)%m == (message%m)):
-                return remainder
-            else:
-                return -1
-
-        else:
-            return -1
-
-
-    
+    #creating deepcopied list of ei_list primes
+    newlist = copy.deepcopy(ei_list)
+    #remove ei-th primes from list
+    newlist.remove(ei_list[i])
+    #recalculation of Si elements without ei-th prime number
+    newsi_list = evaluate_Si( newlist, a, n)
+    #set prod to neutral element 1
     prod = 1
-    #evaluating product of series
-    for j in range(0,len(si)):
-        if(j != i):
-            prod *= (si[j]**int.from_bytes(aux[j].encode(),sys.byteorder))
-    #evaluating product's root
 
-    if((remainder:= __ethRoot(ei[i],prod,N)) == -1):
-        print("Error while evaluating eth root")
+    counter = 0
+    for j in range(len(newsi_list)): 
+        #if ei-th random primes is at index i, skip (one S value is gone)
+        if(counter == i):
+            counter += 1
+        #evaluate opening value
+        prod = (prod*pow(newsi_list[j], messages[counter], n))%n
+        #next message pointer
+        counter += 1
+
+    return prod
+
+#commit fun
+def commit(messages, S, n):
+    #set neutral element c = 1
+    c = 1
+    for i in range(len(messages)):
+        #set c = s1^m1*...*sq^mq
+        c = (c*pow(S[i], messages[i], n))%n
+    return c
+
+
+#divide et impera method
+def evaluate_Si(primes, a, n): 
+    num = len(primes)
+    if num == 1:
+        return [a]
+
+    a1 = []
+    a2 = []
+
+    partial_a1 = a
+    partial_a2 = a
+
+    #for each primes number append a^primes[i] mod n
+    for i in range(math.floor(len(primes)/2)):
+        partial_a2 = pow(partial_a2, primes[i], n)
+        a1.append(primes[i])
+    #for each primes number append a^primes[i] mod n
+    for i in range(math.floor(len(primes)/2), len(primes)):
+        partial_a1 = pow(partial_a1, primes[i], n)
+        a2.append(primes[i])
+
+    #recursive call to self fun
+    l1 = evaluate_Si(a1, partial_a1, n)
+    l2 = evaluate_Si(a2, partial_a2, n)
+
+    si_list = []
+    for element in l1:
+        si_list.append(element)
+    for element in l2:
+        si_list.append(element)
+
+    return si_list
+
+def keyGen(k,qlen, l):
+    #generation of two k/2-bit primes p and q
+    p =number.getPrime(math.floor(k/2))
+    q = number.getPrime(math.floor(k/2))
+    #set n = p*q (rsa)
+    n = p*q
+    #fix a=5 random value
+    a = 5
+
+
+    
+    #list of l+1-bit q random primes
+    random_primes = []
+    while True:
+        #temporary prime
+        tmp = number.getPrime(l + 1)
+        #boolean variable to check if prime number is already used
+        alreadyUsed = False
+        #foreach to check duplicate
+        for num in random_primes:
+            if tmp == num:
+                alreadyUsed = True
+        if alreadyUsed:
+            continue
+        #add prime number if is not duplicated
+        random_primes.append(tmp)
+        #check if are generated q primes 
+        if len(random_primes) == qlen:
+            break
+    
+    #call fun to evaluate Si element
+    si_list = evaluate_Si(random_primes, a, n)
+    return n, random_primes, a, si_list
+
+
+def main():
+    messages = [1234, 5678, 9101112] 
+
+    n, ei_list, a, si_list = keyGen(k=2048,qlen= len(messages),l=25)
+
+    c = commit(messages, si_list, n)
+
+    proof = open(messages, ei_list, a, n, 0)
+    verified = verify(c, messages[0], 0, proof, si_list, ei_list, n)
+    if(verified):
+        print("Verifying worked")
     else:
-        return remainder
-
-
-#verify procedu1re
-def vrfy(C: int, m: str, i: int, proof):
-    #evaluate si^m
-    firstvalue = ((si[i]**int.from_bytes(m.encode(),sys.byteorder))) 
-
-    #evaluate si^m * proof^ei
-    productvalue = firstvalue * (proof**ei[i])
-
-    #evalue product mod N
-    modulevalue = productvalue%(N)
-    #check if commitmentvalue == commitment_evaluated
-    if (C == modulevalue):
-        return True
-    else:
-        return False
+        print("Verifying not worked")
 
 
 
-#message space is {0,1}^l, e.g if l = 1024 bit or 1 KByte and a string is made of 1 byte for 1 character, we can store onlye 128 character
-#getting pubblic parameter pp such as (N,g,si_list,ei_set)
-(N,g,si,ei) = keyGen(k=6,l=8,q=2)
-commitment_value = com(message=["1","2","3"])
-print("Commitment value: " + str(commitment_value))
-proof = open("1",1,aux = ["1","2","3"])
-print("Proof value: " + str(proof))
-print("Verfying output: " + str(vrfy(commitment_value,"1",1,proof)))
-out = "Terminated in {} ms"
-print(out.format(time.time() - start_time))
 
-
+main()
